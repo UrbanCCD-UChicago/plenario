a9cc2367913ffc61478c6dea904e8a4d224f1713
Eric van Zanten <eric@bahai.us>
2013-12-14 06:06:37 -0600

    Added a bit of documentation. Fixed a bug with the way that the query params were constructed

--- app.py
+++ app.py
@@ -34,6 +34,8 @@ def make_query(table, raw_query_params):
     status_code = 200
     query_clauses = []
     valid_query = True
+    args_keys.remove('offset')
+    args_keys.remove('limit')
     for query_param in args_keys:
         try:
             field, operator = query_param.split('__')
