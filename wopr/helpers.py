f6940c365b83f7944f3965029c4b3aaede7b39b6
Eric van Zanten <eric.vanzanten@gmail.com>
2014-05-06 11:26:32 -0500

    Got code written for master table update

--- wopr/helpers.py
+++ wopr/helpers.py
@@ -1,10 +1,12 @@
 import requests
 import os
 from datetime import datetime
-from sqlalchemy import Column, Integer, Table, func, select, Boolean
+from sqlalchemy import Column, Integer, Table, func, select, Boolean, \
+    UniqueConstraint, text
+from sqlalchemy.exc import NoSuchTableError
 from sqlalchemy.dialects.postgresql import TIMESTAMP
 from wopr.database import engine, Base
-from wopr.models import crime_table
+from wopr.models import crime_table, MasterTable
 import gzip
 
 CRIMES = 'https://data.cityofchicago.org/api/views/ijzp-q8t2/rows.csv?accessType=DOWNLOAD'
@@ -27,7 +29,31 @@ def download_crime():
                 f.flush()
     return fpath
 
-def load_raw_crime(fpath=None, tablename='raw_chicago_crimes_all'):
+def dat_crime():
+    # Step Zero: Create dat_crime table
+    try:
+        src_crime_table = Table('src_chicago_crimes_all', Base.metadata, 
+            autoload=True, autoload_with=engine, extend_existing=True)
+    except NoSuchTableError:
+        src_crime_table = src_crime()
+    dat_crime_table = crime_table('dat_chicago_crimes_all', Base.metadata)
+    dat_crime_table.append_column(Column('chicago_crimes_all_row_id', Integer, primary_key=True))
+    dat_crime_table.append_column(Column('start_date', TIMESTAMP, default=datetime.now))
+    dat_crime_table.append_column(Column('end_date', TIMESTAMP, default=None))
+    dat_crime_table.append_column(Column('current_flag', Boolean, default=True))
+    dat_crime_table.append_constraint(UniqueConstraint('id', 'start_date'))
+    dat_crime_table.create(bind=engine)
+    new_cols = ['start_date', 'end_date', 'current_flag', 'chicago_crimes_row_id']
+    dat_ins = dat_crime_table.insert()\
+        .from_select(
+            [c.name for c in dat_crime_table.columns.keys() if c.name not in new_cols],
+            select([c for c in src_crime_table.columns])
+        )
+    conn = engine.connect()
+    conn.execute(dat_ins)
+    return dat_crime_table
+
+def raw_crime(fpath=None, tablename='raw_chicago_crimes_all'):
     # Step One: Load raw downloaded data
     if not fpath:
         fpath = download_crime()
@@ -46,12 +72,15 @@ def load_raw_crime(fpath=None, tablename='raw_chicago_crimes_all'):
             latitude, longitude, location) FROM STDIN WITH \
             (FORMAT CSV, HEADER true, DELIMITER ',')" % tablename, f)
     conn.commit()
-    return 'Created raw crime table'
+    return raw_crime_table
 
-def dedupe_crime_table():
+def dedupe_crime():
     # Step Two: Find dubplicate records by id
-    raw_crime_table = Table('raw_chicago_crimes_all', Base.metadata, 
-        autoload=True, autoload_with=engine, extend_existing=True)
+    try:
+        raw_crime_table = Table('raw_chicago_crimes_all', Base.metadata, 
+            autoload=True, autoload_with=engine, extend_existing=True)
+    except NoSuchTableError:
+        raw_crime_table = raw_crime(tablename='raw_chicago_crimes_all')
     dedupe_crime_table = Table('dedup_chicago_crimes_all', Base.metadata,
         Column('dup_row_id', Integer, primary_key=True),
         extend_existing=True)
@@ -67,12 +96,18 @@ def dedupe_crime_table():
     conn.execute(ins)
     return dedupe_crime_table
 
-def create_src_table():
+def src_crime():
     # Step Three: Create New table with unique ids
-    raw_crime_table = Table('raw_chicago_crimes_all', Base.metadata, 
-        autoload=True, autoload_with=engine, extend_existing=True)
-    dedupe_crime_table = Table('dedup_chicago_crimes_all', Base.metadata, 
-        autoload=True, autoload_with=engine, extend_existing=True)
+    try:
+        raw_crime_table = Table('raw_chicago_crimes_all', Base.metadata, 
+            autoload=True, autoload_with=engine, extend_existing=True)
+    except NoSuchTableError:
+        raw_crime_table = raw_crime()
+    try:
+        dedupe_crime_table = Table('dedup_chicago_crimes_all', Base.metadata, 
+            autoload=True, autoload_with=engine, extend_existing=True)
+    except NoSuchTableError:
+        dedupe_crime_table = dedupe_crime()
     src_crime_table = crime_table('src_chicago_crimes_all', Base.metadata)
     src_crime_table.drop(bind=engine, checkfirst=True)
     src_crime_table.create(bind=engine)
@@ -86,31 +121,21 @@ def create_src_table():
     conn.execute(ins)
     return src_crime_table
 
-def insert_new_crimes():
-    src_crime_table = Table('src_chicago_crimes_all', Base.metadata, 
-        autoload=True, autoload_with=engine, extend_existing=True)
+def new_crime():
+    # Step Four: Insert new crimes into dat table
     try:
         dat_crime_table = Table('dat_chicago_crimes_all', Base.metadata, 
             autoload=True, autoload_with=engine, extend_existing=True)
     except NoSuchTableError:
         # bust this out into a function
-        dat_crime_table = crime_table('dat_chicago_crimes_all', Base.metadata)
-        dat_crime_table.append_column(Column('chicago_crimes_all_row_id', Integer, primary_key=True))
-        dat_crime_table.append_column(Column('start_date', TIMESTAMP, default=datetime.now))
-        dat_crime_table.append_column(Column('end_date', TIMESTAMP, default=None))
-        dat_crime_table.append_column(Column('current_flag', Boolean, default=True))
-        # Need to add uniqueness constraint (pk, start_date)
-        dat_crime_table.create(bind=engine)
-        new_cols = ['start_date', 'end_date', 'current_flag', 'chicago_crimes_row_id']
-        dat_ins = dat_crime_table.insert()\
-            .from_select(
-                [c.name for c in dat_crime_table.columns.keys() if c.name not in new_cols],
-                select([c for c in src_crime_table.columns])
-            )
-        conn = engine.connect()
-        conn.execute(dat_ins)
+        dat_crime_table = dat_crime()
+    try:
+        src_crime_table = Table('src_chicago_crimes_all', Base.metadata, 
+            autoload=True, autoload_with=engine, extend_existing=True)
+    except NoSuchTableError:
+        src_crime_table = src_crime()
     new_crime_table = Table('new_chicago_crimes_all', Base.metadata, 
-        Column('id', Integer, primary_key=True)
+        Column('id', Integer, primary_key=True),
         extend_existing=True)
     new_crime_table.drop(bind=engine, checkfirst=True)
     new_crime_table.create(bind=engine)
@@ -118,11 +143,40 @@ def insert_new_crimes():
         .from_select(
             ['id'],
             select([src_crime_table.c.id])\
-                .select_frome(src_crime_table.outerjoin(dat_crime_table.c.id))\
+                .select_from(src_crime_table.join(dat_crime_table, 
+                    src_crime_table.c.id == dat_crime_table.c.id, isouter=True))\
                 .where(dat_crime_table.c.chicago_crimes_all_row_id != None)
         )
     conn = engine.connect()
     conn.execute(ins)
     return new_crime_table
 
-
+def update_master():
+    # Step Five: Update Master table
+    try:
+        dat_crime_table = Table('dat_chicago_crimes_all', Base.metadata, 
+            autoload=True, autoload_with=engine, extend_existing=True)
+    except NoSuchTableError:
+        dat_crime_table = dat_crime()
+    try:
+        new_crime_table = Table('new_chicago_crimes_all', Base.metadata, 
+            autoload=True, autoload_with=engine, extend_existing=True)
+    except NoSuchTableError:
+        new_crime_table = new_crime()
+    col_names = ['start_date', 'end_date', 'current_flag', 'location', 'latitude', 'longitude']
+    cols = [c for c in dat_crime_table.columns if c.name in col_names]
+    cols.append(dat_crime_table.c.orig_date.label('obs_date'))
+    cols.append(text("NULL AS obs_ts"))
+    cols.append(text("'chicago_crimes_all' AS dataset_name"))
+    cols.append(text("ST_PointFromText('POINT(' || dat_chicago_crimes_all.longitude || ' ' || dat_chicago_crimes_all.latitude || ')') as location_geom"))
+    cols.append(dat_crime_table.c.chicago_crimes_all_row_id.label('dataset_row_id'))
+    ins = MasterTable.insert()\
+        .from_select(
+            MasterTable.columns.keys(),
+            select(cols)\
+                .select_from(dat_crime_table.join(new_crime_table, 
+                    dat_crime_table.c.id == new_crime_table.c.id))
+        )
+    print ins
+    conn = engine.connect()
+    conn.execute(ins)
--- wopr/models.py
+++ wopr/models.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import Column, Integer, String, Boolean, Table, types
+from sqlalchemy import Column, Integer, String, Boolean, Table
 from sqlalchemy.dialects.postgresql import TIMESTAMP, DOUBLE_PRECISION
 from geoalchemy2 import Geometry
 from sqlalchemy.ext.declarative import declarative_base
@@ -13,19 +13,11 @@ from wopr.database import Base, engine
 MasterTable = Table('dat_master', Base.metadata,
     autoload=True, autoload_with=engine)
 
-class NullInteger(types.TypeDecorator):
-    impl = types.Integer
-    def process_result_value(self, value, engine):
-        try:
-            return int(value)
-        except ValueError:
-            return None
-
 def crime_table(name, metadata):
     table = Table(name, metadata,
             Column('id', Integer),
             Column('case_number', String(length=10)),
-            Column('date', TIMESTAMP),
+            Column('orig_date', TIMESTAMP),
             Column('block', String(length=50)),
             Column('iucr', String(length=10)),
             Column('primary_type', String(length=100)),
@@ -35,12 +27,12 @@ def crime_table(name, metadata):
             Column('domestic', Boolean),
             Column('beat', String(length=10)),
             Column('district', String(length=5)),
-            Column('ward', NullInteger),
+            Column('ward', Integer),
             Column('community_area', String(length=10)),
             Column('fbi_code', String(length=10)),
-            Column('x_coordinate', NullInteger, nullable=True),
-            Column('y_coordinate', NullInteger, nullable=True),
-            Column('year', NullInteger),
+            Column('x_coordinate', Integer, nullable=True),
+            Column('y_coordinate', Integer, nullable=True),
+            Column('year', Integer),
             Column('updated_on', TIMESTAMP, default=None),
             Column('latitude', DOUBLE_PRECISION(precision=53)),
             Column('longitude', DOUBLE_PRECISION(precision=53)),
