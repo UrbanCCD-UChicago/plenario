3cc3f1e707f00b6ec7c65ec390f206ce58bc5a64
Derek Eder <derek.eder@gmail.com>
2014-06-17 10:49:58 -0500

    updated analytics tracking code, minor text edits to API

--- wopr/static/js/analytics_lib.js
+++ wopr/static/js/analytics_lib.js
@@ -10,7 +10,7 @@
  *
  */
 
-var analyticsTrackingCode = 'UA-46191637-1'; //enter your tracking code here
+var analyticsTrackingCode = 'UA-39054699-5'; //enter your tracking code here
 
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', analyticsTrackingCode]);
