b0d44c39ff013391df0109e6430e49328680f8fb
Eric van Zanten <eric.vanzanten@gmail.com>
2014-08-22 17:04:52 -0500

    Able to load Census Blocks. Should be able to load most any shapefile

--- plenario/utils/shapefile_helpers.py
+++ plenario/utils/shapefile_helpers.py
@@ -4,8 +4,10 @@ import re
 from unicodedata import normalize
 from datetime import datetime, date
 from sqlalchemy import Column, Integer, Table, func, select, Boolean, \
-    Date, DateTime, UniqueConstraint, text, and_, or_
+    Date, DateTime, UniqueConstraint, text, and_, or_, Float, String
 from sqlalchemy.dialects.postgresql import TIMESTAMP
+from sqlalchemy.exc import NoSuchTableError
+from geoalchemy2 import Geometry
 from plenario.database import task_engine as engine, Base
 from plenario.models import shp2table
 from plenario.settings import AWS_ACCESS_KEY, AWS_SECRET_KEY, S3_BUCKET
@@ -14,12 +16,21 @@ from boto.s3.key import Key
 import gzip
 import zipfile
 import shapefile
-from shapely.geometry import shape, Polygon, MultiPolygon
+from shapely.geometry import shape, Polygon, MultiPolygon, asShape
 import json
-import pyproj
+# import pyproj
 from cStringIO import StringIO
+from itertools import izip_longest
+from urlparse import urlparse
 
 ENDPOINT = 'http://www2.census.gov/geo/tiger/TIGER2010'
+
+TYPE_MAP = {
+    'C': String,
+    'N': Float,
+    'L': Float,
+    'D': TIMESTAMP,
+}
 
 class PlenarioShapeETL(object):
     """ 
@@ -30,13 +41,20 @@ class PlenarioShapeETL(object):
     def __init__(self, meta):
         for k,v in meta.items():
             setattr(self, k, v)
-        
-        s3_path = '%s/%s.zip' % \
-            (self.dataset_name, datetime.now().strftime('%Y-%m-%dT%H:%M:%S'))
+        fname = urlparse(self.source_url).path.split('/')[-1]
+        s3_path = '%s/%s_%s.zip' % \
+            (self.dataset_name, 
+                fname, datetime.now().strftime('%Y-%m-%dT%H:%M:%S'))
         s3conn = S3Connection(AWS_ACCESS_KEY, AWS_SECRET_KEY)
         bucket = s3conn.get_bucket(S3_BUCKET)
         self.s3_key = Key(bucket)
         self.s3_key.key = s3_path
+
+    def add(self):
+        self._download()
+        self._load_shapefile()
+        self._get_or_create_table()
+        self._insert_data()
 
     def _download(self):
         """ 
@@ -51,6 +69,85 @@ class PlenarioShapeETL(object):
         s.seek(0)
         self.s3_key.set_contents_from_file(s)
         self.s3_key.make_public()
+    
+    def _load_shapefile(self):
+        """
+        Import a shapefile into the PostGIS database
+        """
+        # Open the shapefile with fiona.
+        content = StringIO()
+        file_contents = self.s3_key.get_contents_to_file(content)
+        content.seek(0)
+        shp = StringIO()
+        dbf = StringIO()
+        shx = StringIO()
+        with zipfile.ZipFile(content, 'r') as f:
+            for name in f.namelist():
+                if name.endswith('.shp'):
+                    shp.write(f.read(name))
+                if name.endswith('.shx'):
+                    shx.write(f.read(name))
+                if name.endswith('.dbf'):
+                    dbf.write(f.read(name))
+        shp.seek(0)
+        shx.seek(0)
+        dbf.seek(0)
+        shape_reader = shapefile.Reader(shp=shp, dbf=dbf, shx=shx)
+        self.fields = shape_reader.fields[1:]
+        self.records = shape_reader.shapeRecords()
+
+    def _get_or_create_table(self):
+        try:
+            self.table = Table(self.dataset_name, Base.metadata, 
+                autoload=True, autoload_with=engine, extend_existing=True)
+            self.multipolygon = False
+            if self.table.columns['geom'].type.geometry_type == 'MULTIPOLYGON':
+                self.multipolygon = True
+        except NoSuchTableError:
+            columns = []
+            for field in self.fields:
+                fname, d_type, f_len, d_len = field
+                col_type = TYPE_MAP[d_type]
+                kwargs = {}
+                if d_type == 'C':
+                    col_type = col_type(f_len)
+                elif d_type == 'N':
+                    col_type = col_type(d_len)
+                if fname.lower() == self.business_key:
+                    kwargs['primary_key'] = True
+                columns.append(Column(fname.lower(), col_type, **kwargs))
+            self.multipolygon = False
+            for record in self.records:
+                geo_type = record.shape.__geo_interface__['type']
+                if 'multi' in geo_type.lower():
+                    self.multipolygon = True
+            geo_type = 'POLYGON'
+            if self.multipolygon:
+                geo_type = 'MULTIPOLYGON'
+            columns.append(Column('geom', Geometry(geo_type)))
+            self.table = Table(self.dataset_name, Base.metadata, *columns)
+            self.table.create(engine, checkfirst=True)
+
+    def _insert_data(self):
+        fields = self.table.columns.keys()
+        values = []
+        for record in self.records:
+            d = {}
+            for k,v in zip(fields, record.record):
+                d[k] = v
+            geom = asShape(record.shape.__geo_interface__)
+            if self.multipolygon and geom.geom_type.lower() == 'polygon':
+                geom = MultiPolygon([geom])
+            d['geom'] = 'SRID=4326;%s' % geom.wkt
+            values.append(d)
+        ins = self.table.insert()
+        conn = engine.contextual_connect()
+        conn.execute(ins, values)
+        conn.close()
+
+def grouper(iterable, n, fillvalue=None):
+    args = [iter(iterable)] * n
+    return izip_longest(*args, fillvalue=fillvalue)
 
 def transform_proj(geom, source, target=4326):
     """Transform a geometry's projection.
@@ -77,73 +174,3 @@ def transform_proj(geom, source, target=4326):
 def grouper(iterable, n, fillvalue=None):
     args = [iter(iterable)] * n
     return izip_longest(*args, fillvalue=fillvalue)
-
-
-
-def import_shapefile(file_contents, name):
-    """Import a shapefile into the PostGIS database
-
-    Keyword arguments:
-    fpath -- path to a zipfile to be extracted
-    name -- name given to the newly created table
-    force_multipoly -- enforce that the gemoetries are multipolygons
-    proj -- source projection spec (EPSG code or Proj$ string)
-    """
-    # Open the shapefile with fiona.
-    content = StringIO(file_contents)
-    shp = StringIO()
-    dbf = StringIO()
-    shx = StringIO()
-    with zipfile.Zipfile(content, 'r') as shp:
-        for name in f.namelist():
-            if name.endswith('.shp'):
-                shp.write(shp.read(name))
-            if name.endswith('.shx'):
-                shx.write(f.read(name))
-            if name.endswith('.dbf'):
-                dbf.write(f.read(name))
-    shp.seek(0)
-    shx.seek(0)
-    dbf.seek(0)
-    shape_reader = shapefile.Reader(shp=shp, dbf=dbf, shx=shx)
-    records = shape_reader.shapeRecords()
-    record_groups = grouper(records, 1000)
-    print record_groups.next()
-   # shp_table = shp2table(name, Base.metadata, shp.schema,
-   #     force_multipoly=force_multipoly)
-   # shp_table.drop(bind=engine, checkfirst=True)
-   # shp_table.append_column(Column('row_id', Integer, primary_key=True))
-   # shp_table.create(bind=engine)
-   # features = []
-   # count = 0
-   # for r in shp:
-   #     # ESRI shapefile don't contemplate multipolygons, i.e. the geometry
-   #     # type is polygon even if multipolygons are contained.
-   #     # If and when the 1st multipoly is encountered, the table is
-   #     # re-initialized.
-   #     if not force_multipoly and r['geometry']['type'] == 'MultiPolygon':
-   #         return import_shapefile(fpath, name, force_multipoly=True, proj=proj)
-   #     row_dict = dict((k.lower(), v) for k, v in r['properties'].iteritems())
-   #     # GeoJSON intermediate representation
-   #     geom_json = json.loads(str(r['geometry']).replace('\'', '"')\
-   #                            .replace('(', '[').replace(')', ']'))
-   #     # If the projection is not long/lat (WGS84 - EPGS:4326), transform.
-   #     if proj != 4326:
-   #         geom_json['coordinates'] = transform_proj(geom_json['coordinates'], proj, 4326)
-   #     # Shapely intermediate representation, used to obtained the WKT
-   #     geom = shape(geom_json)
-   #     if force_multipoly and r['geometry']['type'] != 'MultiPolygon':
-   #         geom = MultiPolygon([geom])
-   #     row_dict['geom'] = 'SRID=4326;{0}'.format(geom.wkt)
-   #     features.append(row_dict)
-   #     count += 1
-   #     # Buffer DB writes
-   #     if not count % 1000:
-   #         ins = shp_table.insert(features)
-   #         conn = engine.contextual_connect()
-   #         conn.execute(ins)
-   #         features = []
-   # ins = shp_table.insert(features)
-   # conn = engine.contextual_connect()
-   # conn.execute(ins)
-    return 'Table {0} created from shapefile'.format(name)
