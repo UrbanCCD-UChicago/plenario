3979a6be6ffec453be7309bb8227ff9949616ced
Derek Eder <derek.eder@gmail.com>
2014-09-15 11:14:16 -0500

    removed terms of use page and link, closes #70

--- plenario/views.py
+++ plenario/views.py
@@ -197,8 +197,3 @@ def check_update(task_id):
     resp = make_response(json.dumps(r))
     resp.headers['Content-Type'] = 'application/json'
     return resp
-
-
-@views.route('/terms')
-def terms_view():
-    return render_template('terms.html')
