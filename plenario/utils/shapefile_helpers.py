a9fa0cb37abbb153d81d9237516f649b16d7b120
Michael Castelle <mc-github@perquackey.com>
2014-09-17 13:58:18 -0500

    First pass at providing local ETL support in absence of S3

--- plenario/utils/shapefile_helpers.py
+++ plenario/utils/shapefile_helpers.py
@@ -9,8 +9,8 @@ from sqlalchemy.dialects.postgresql import TIMESTAMP
 from sqlalchemy.exc import NoSuchTableError
 from geoalchemy2 import Geometry
 from plenario.database import task_engine as engine, Base
-from plenario.settings import AWS_ACCESS_KEY, AWS_SECRET_KEY, S3_BUCKET
-from boto.s3.connection import S3Connection
+from plenario.settings import AWS_ACCESS_KEY, AWS_SECRET_KEY, S3_BUCKET, DATA_DIR
+from boto.s3.connection import S3Connection, S3ResponseError
 from boto.s3.key import Key
 import gzip
 import zipfile
@@ -51,13 +51,31 @@ class PlenarioShapeETL(object):
         for k,v in meta.items():
             setattr(self, k, v)
         fname = urlparse(self.source_url).path.split('/')[-1]
-        s3_path = '%s/%s_%s.zip' % \
-            (self.dataset_name, 
-                fname, datetime.now().strftime('%Y-%m-%dT%H:%M:%S'))
-        s3conn = S3Connection(AWS_ACCESS_KEY, AWS_SECRET_KEY)
-        bucket = s3conn.get_bucket(S3_BUCKET)
-        self.s3_key = Key(bucket)
-        self.s3_key.key = s3_path
+        self.s3_key = None
+        # If plenario.settings.AWS_ACCESS_KEY is undefined, or if we have trouble connecting to AWS,
+        # then use DATA_DIR to save and extract shapefile data.
+        if (AWS_ACCESS_KEY != ''):
+            s3_path = '%s/%s_%s.zip' % \
+                      (self.dataset_name, 
+                       fname, datetime.now().strftime('%Y-%m-%dT%H:%M:%S'))
+            try:
+                s3conn = S3Connection(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+                bucket = s3conn.get_bucket(S3_BUCKET)
+                self.s3_key = Key(bucket)
+                self.s3_key.key = s3_path
+            except S3ResponseError, e:
+                # XX: When this happens, we should log a more serious message
+                print "Failed to connect to S3 for filename '%s', trying to init locally" % fname
+                self._init_local(fname)
+        else:
+            self._init_local(fname)
+
+
+    def _init_local(self, fname):
+        print "PlenarioShapeETL._init_local('%s')" % fname
+        # use DATA_DIR to store source_url file
+        self.fname = fname
+        self.data_dir = DATA_DIR
 
     def add(self):
         self._download()
@@ -70,18 +88,37 @@ class PlenarioShapeETL(object):
         Download and cache file on S3
         """
         r = requests.get(self.source_url, stream=True)
-        s = StringIO()
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:
-                s.write(chunk)
-                s.flush()
-        s.seek(0)
-        self.s3_key.set_contents_from_file(s)
-        self.s3_key.make_public()
-    
+
+        if (self.s3_key):
+
+            s = StringIO()
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:
+                    s.write(chunk)
+                    s.flush()
+            s.seek(0)
+
+            self.s3_key.set_contents_from_file(s)
+            self.s3_key.make_public()
+        else: # write locally
+            self.fpath = os.path.join(self.data_dir, self.fname)
+            if not os.path.exists(self.fpath):
+                f = open(self.fpath, 'wb')
+                print "opened " , self.fpath
+                for chunk in r.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
+                        f.flush()
+                f.close() # Explicitly close before re-opening to read.
+                
+
     def _load_shapefile(self):
         content = StringIO()
-        file_contents = self.s3_key.get_contents_to_file(content)
+        if (self.s3_key):
+            file_contents = self.s3_key.get_contents_to_file(content)
+        else:
+            file_contents = open(self.fpath, 'r')
+            content.write(file_contents.read())
         content.seek(0)
         shp = StringIO()
         dbf = StringIO()
