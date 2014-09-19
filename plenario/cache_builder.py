eb7461009eeb45c6ce8cc8b598018bce798ef08a
Eric van Zanten <eric.vanzanten@gmail.com>
2014-09-19 15:13:11 -0500

    :zap: PARALLEL-I-ZATION :zap:

--- plenario/cache_builder.py
+++ plenario/cache_builder.py
@@ -2,6 +2,7 @@ import sys
 import requests
 import json
 from datetime import date, datetime, timedelta
+from dateutil import parser
 
 longest_request_ms = 0
 longest_request = ''
@@ -14,55 +15,56 @@ def set_longest_request(val):
     global longest_request
     longest_request = val
 
-def main():
-
-    cache_end_date = date(2014,9,26)
-
-    for d in range (8):
-        cache_end_date = cache_end_date - timedelta(days=1)
-        cache_start_date = cache_end_date - timedelta(days=180)
-        fetch_for_dates(cache_start_date,cache_end_date)
-
-    print '\n\nlongest request:'
-    print longest_request
-
 def fetch_for_dates(date_start, date_end):
     base_url = 'http://plenar.io'
 
     print '--- fetching for dates %s - %s ---' % (date_start, date_end)
 
-    fetch_url('%s/v1/api/timeseries/?obs_date__le=%s&obs_date__ge=%s&agg=week' % (base_url,date_end, date_start))
+    yield '%s/v1/api/timeseries/?obs_date__le=%s&obs_date__ge=%s&agg=week' % (base_url,date_end, date_start)
     datasets = fetch_url('%s/v1/api/datasets' % base_url)
 
     for d in datasets['objects']:
-        fetch_url('%s/v1/api/fields/%s' % (base_url, d['dataset_name']))
+        yield '%s/v1/api/fields/%s' % (base_url, d['dataset_name'])
 
     for d in datasets['objects']:
-        fetch_url('%s/v1/api/datasets/?dataset_name=%s' % (base_url, d['dataset_name']))
+        yield '%s/v1/api/datasets/?dataset_name=%s' % (base_url, d['dataset_name'])
 
     for d in datasets['objects']:
-        fetch_url('%s/v1/api/grid/?obs_date__le=%s&obs_date__ge=%s&agg=week&dataset_name=%s&resolution=500' % (base_url, date_end, date_start, d['dataset_name']))
+        yield '%s/v1/api/grid/?obs_date__le=%s&obs_date__ge=%s&agg=week&dataset_name=%s&resolution=500' % (base_url, date_end, date_start, d['dataset_name'])
 
     for d in datasets['objects']:
-        fetch_url('%s/v1/api/detail-aggregate/?obs_date__le=%s&obs_date__ge=%s&agg=week&dataset_name=%s' % (base_url, date_end, date_start, d['dataset_name']))
+        yield '%s/v1/api/detail-aggregate/?obs_date__le=%s&obs_date__ge=%s&agg=week&dataset_name=%s' % (base_url, date_end, date_start, d['dataset_name'])
 
     for d in datasets['objects']:
-        fetch_url('%s/v1/api/detail/?obs_date__le=%s&obs_date__ge=%s&agg=week&dataset_name=%s' % (base_url, date_end, date_start, d['dataset_name']))
+        yield '%s/v1/api/detail/?obs_date__le=%s&obs_date__ge=%s&agg=week&dataset_name=%s' % (base_url, date_end, date_start, d['dataset_name'])
 
 def fetch_url(url):
     try:
         r = requests.get(url)
         p = 'GET "%s" | %s | %sms' % (url, r.status_code, (r.elapsed.microseconds / 1000))
-        print p
         if longest_request_ms < r.elapsed.microseconds:
             set_longest_request_ms(r.elapsed.microseconds)
             set_longest_request(p)
-        return r.json()
-    except:
-        e = sys.exc_info()[0]
-        print e
+        try:
+            return r.json()
+        except ValueError:
+            print 'Junk response %s' % r.content[:100]
+            return None
+    except requests.exceptions.ConnectTimeout, e:
+        print 'Connection timeout %s' % url
+        return None
+    except requests.exceptions.Timeout, e:
+        print 'Request timeout %s' % url
         return None
 
 
 if __name__ == "__main__":
-    main()
+    from multiprocessing import Pool
+    pool = Pool(processes=10)
+    cache_end_date = parser.parse(sys.argv[1])
+    for d in range (8):
+        cache_end_date = cache_end_date - timedelta(days=1)
+        cache_start_date = cache_end_date - timedelta(days=180)
+        pool.map(fetch_url, list(fetch_for_dates(cache_start_date, cache_end_date)))
+    print '\n\nlongest request:'
+    print longest_request
