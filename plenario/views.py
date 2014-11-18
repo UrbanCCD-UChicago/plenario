aea9b8fea1ee69ac12b3442e132c53d471169222
Derek Eder <derek.eder@gmail.com>
2014-11-18 10:49:14 -0600

    removed print statement for @mccc

--- plenario/views.py
+++ plenario/views.py
@@ -156,8 +156,6 @@ def add_dataset_to_metatable(request, url, dataset_id, dataset_info, socrata_sou
         'is_socrata_source': socrata_source
     }
 
-    print d
-
     # add this to meta_master
     md = MetaTable(**d)
     session.add(md)
