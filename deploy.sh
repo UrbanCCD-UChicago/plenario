485bffb6f088e1e4ea4cecc64099a6d16a8e21d1
Will Engler <Engler.Will@gmail.com>
2016-01-13 13:33:36 -0600

    Nope nope nope. Don't leak credentials.

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-echo $DEPLOY_KEY > pkey.pem
-chmod 600 pkey.pem
+UPDATE_CMDS="cd /home/urbanccd/plenario ; sudo git checkout master; sudo git pull; sudo supervisorctl restart all; exit;"
+SSH_SETTINGS="ssh"
 
-export SSHPASS=""
-
-UPDATE_CMDS="cd /home/urbanccd/plenario; sudo git checkout master; sudo git pull; sudo supervisorctl restart all; exit;"
-SSH_SETTINGS="sshpass -e ssh -o StrictHostKeyChecking=no"
-CONN_SETTINGS="-i pkey.pem"
-
-$SSH_SETTINGS ubuntu@plenar.io $CONN_SETTINGS $UPDATE_CMDS
-$SSH_SETTINGS ubuntu@52.70.151.225 $CONN_SETTINGS $UPDATE_CMDS
-
-rm pkey.pem
+cd /home/urbanccd/plenario
+sudo git checkout master
+sudo git pull
+sudo supervisorctl restart all
+ssh ubuntu@10.0.1.100 $UPDATE_CMDS exit;
