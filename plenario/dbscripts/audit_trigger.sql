b95f1576d55777c11a4a7a5924f00c882e2bd8a8
Ubuntu <ubuntu@ip-172-31-7-130.ec2.internal>
2016-01-17 00:20:34 +0000

    Initial audit trigger

---
+++ plenario/dbscripts/audit_trigger.sql
@@ -0,0 +1,57 @@
+CREATE SCHEMA audit;
+
+COMMENT ON SCHEMA audit IS 'Out-of-table audit/history logging tables and trigger functions';
+
+CREATE OR REPLACE FUNCTION audit.if_modified() RETURNS TRIGGER AS $body$
+DECLARE
+    temp_row RECORD; -- a temporary variable used on updates/deletes
+    v_count int;
+    v_sql text;
+BEGIN
+    IF TG_WHEN <> 'AFTER' THEN
+        RAISE EXCEPTION 'audit.if_modified() may only run as an AFTER trigger';
+    END IF;
+
+    --if not exists history table create it
+    v_table = TG_TABLE_NAME::regclass || '_history'
+    v_sql = 'SELECT ' || '  count(1) ' || 'FROM ' || '  information_schema.tables ' || 'WHERE ' || E'  table_name=\'' || v_table || E'\'';
+    
+    EXECUTE v_sql INTO v_count;
+    IF (v_count == 0)
+        v_sql = '
+	CREATE TABLE ' || TG_TABLE_NAME::regclass || '_history' || '(
+	    action_tstamp_tx TIMESTAMP WITH TIME ZONE NOT NULL,
+	    action CHAR(1) NOT NULL CHECK (action IN ('I','D','U', 'T')),
+	    row_data hstore,
+	)';
+
+       EXECUTE v_sql;
+    END IF
+    --else
+
+     temp_row = ROW(
+        CURRENT_TIMESTAMP,                            -- action_tstamp_tx
+        SUBSTRING(TG_OP,1,1),                         -- action
+        NULL                                          -- row_data
+        );
+ 
+    IF (TG_OP = 'UPDATE' AND TG_LEVEL = 'ROW') THEN
+        temp_row.row_data = OLD;
+    ELSIF (TG_OP = 'DELETE' AND TG_LEVEL = 'ROW') THEN
+        temp_row.row_data = OLD;
+    ELSIF (TG_OP = 'INSERT' AND TG_LEVEL = 'ROW') THEN
+        temp_row.row_data = NEW;
+    ELSIF (TG_LEVEL = 'STATEMENT' AND TG_OP IN ('INSERT','UPDATE','DELETE','TRUNCATE')) THEN
+        temp_row.statement_only = 't';
+    ELSE
+        RAISE EXCEPTION '[audit.if_modified] - Trigger func added as trigger for unhandled case: %, %',TG_OP, TG_LEVEL;
+        RETURN NULL;
+    END IF;
+    EXECUTE 'INSERT INTO ' || TG_TABLE_NAME::regclass || '_history VALUES (temp_row.*)'; 
+    
+    RETURN NULL;
+END;
+$body$
+LANGUAGE plpgsql
+SECURITY DEFINER
+SET search_path = pg_catalog, public;
