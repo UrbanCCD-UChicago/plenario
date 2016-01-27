5035ea7a88ac4e2dd2cfa37bec3f5dceba1c14bc
Will Engler <Engler.Will@gmail.com>
2016-01-27 13:16:33 -0600

    Only display datasets that have been ingested

--- plenario/api/point.py
+++ plenario/api/point.py
@@ -665,7 +665,7 @@ def meta():
                 m.longitude, m.observed_date, m.human_name, m.dataset_name,
                 m.update_freq, ST_AsGeoJSON(m.bbox) as bbox
         FROM meta_master AS m
-        WHERE m.approved_status = 'true'
+        WHERE m.date_added IS NOT NULL
     '''
 
     if dataset_name:
