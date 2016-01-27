8487df5e99f1db77d7cf062e2eac88a3569f697a
Andrew Yang <ayang16@uchicago.edu>
2016-01-27 14:28:35 -0600

    also added files described in last commit and neighborhood zipfile

---
+++ tests/test_fixtures/base_test.py
@@ -0,0 +1,123 @@
+import json
+import os
+import unittest
+import urllib
+import zipfile
+import csv
+from StringIO import StringIO
+
+import unittest
+
+from tests.test_fixtures.point_meta import flu_shot_meta, landmarks_meta, flu_path, landmarks_path, \
+    crime_meta, crime_path
+from plenario.models import MetaTable
+from plenario.database import session
+from plenario.etl.point import PlenarioETL
+
+from init_db import init_meta
+from plenario import create_app
+from plenario.database import session, app_engine as engine
+from plenario.etl.shape import ShapeETL
+from plenario.models import ShapeMetadata
+from plenario.utils.shapefile import Shapefile
+
+pwd = os.path.dirname(os.path.realpath(__file__))
+#fixtures_path = os.path.join(pwd, 'test_fixtures')
+#FIXTURE_PATH = os.path.join(pwd, 'test_fixtures')
+
+fixtures_path = pwd
+FIXTURE_PATH = pwd
+
+print fixtures_path
+print FIXTURE_PATH
+
+def ingest_from_fixture(fixture_meta, fname):
+    md = MetaTable(**fixture_meta)
+    session.add(md)
+    session.commit()
+    path = os.path.join(fixtures_path, fname)
+    point_etl = PlenarioETL(md, source_path=path)
+    point_etl.add()
+
+
+def drop_tables(table_names):
+    drop_template = 'DROP TABLE IF EXISTS {};'
+    command = ''.join([drop_template.format(table_name) for table_name in table_names])
+    print command
+    session.execute(command)
+    session.commit()
+
+class Fixture(object):
+    def __init__(self, human_name, file_name):
+        self.human_name = human_name
+        self.table_name = ShapeMetadata.make_table_name(human_name)
+        self.path = os.path.join(FIXTURE_PATH, file_name)
+        self.update_freq = 'yearly'
+
+fixtures = {
+    'city': Fixture(human_name=u'Chicago City Limits',
+                    file_name='chicago_city_limits.zip'),
+    'streets': Fixture(human_name=u'Pedestrian Streets',
+                       file_name='chicago_pedestrian_streets.zip'),
+    'zips': Fixture(human_name=u'Zip Codes',
+                    file_name='chicago_zip_codes.zip'),
+    'neighborhoods': Fixture(human_name=u'Chicago Neighborhoods',
+                             file_name='chicago_neighborhoods.zip')
+}
+
+class BasePlenarioTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls, shutdown=False):
+
+        # Remove tables that we're about to recreate.
+        # This doesn't happen in teardown because I find it helpful to inspect them in the DB after running the tests.
+        meta_table_names = ['meta_shape']
+        fixture_table_names = [fixture.table_name for key, fixture in fixtures.iteritems()]
+
+        drop_tables(meta_table_names + fixture_table_names)
+
+        # Re-add meta tables
+        init_meta()
+
+        # Fully ingest the fixtures
+        BasePlenarioTest.ingest_fixture(fixtures['city'])
+        BasePlenarioTest.ingest_fixture(fixtures['streets'])
+        BasePlenarioTest.ingest_fixture(fixtures['zips'])
+        BasePlenarioTest.ingest_fixture(fixtures['neighborhoods'])
+
+        # Add a dummy dataset to the metadata without ingesting a shapefile for it
+        cls.dummy_name = ShapeMetadata.add(human_name=u'Dummy Name',
+                                           source_url=None,
+                                           update_freq='yearly').dataset_name
+        session.commit()
+
+        tables_to_drop = [
+            'flu_shot_clinics',
+            'landmarks',
+            'crimes',
+            'meta_master'
+        ]
+        drop_tables(tables_to_drop)
+
+        init_meta()
+
+        ingest_from_fixture(flu_shot_meta, flu_path)
+        ingest_from_fixture(landmarks_meta, landmarks_path)
+        ingest_from_fixture(crime_meta, crime_path)
+
+        cls.app = create_app().test_client()
+
+        '''/detail'''
+
+    @staticmethod
+    def ingest_fixture(fixture):
+        # Add the fixture to the metadata first
+        shape_meta = ShapeMetadata.add(human_name=fixture.human_name,
+                                       source_url=None,
+                                       update_freq=fixture.update_freq)
+        session.commit()
+        # Bypass the celery task and call on a ShapeETL directly
+        ShapeETL(meta=shape_meta, source_path=fixture.path).ingest()
+        return shape_meta
+
