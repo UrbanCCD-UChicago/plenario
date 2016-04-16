53daf358ef2a6941c16c1fbb6d0903bd81db1afc
Will Engler <Engler.Will@gmail.com>
2016-04-16 14:56:53 -0500

    Checkpoint: App server requests working

--- plenario/__init__.py
+++ plenario/__init__.py
@@ -1,88 +1,88 @@
-# from flask import Flask, render_template, redirect, url_for, request
-# from raven.contrib.flask import Sentry
-# from plenario.database import session as db_session
-# from plenario.models import bcrypt
-# from plenario.api import api, cache
-# from plenario.auth import auth, login_manager
-# from plenario.views import views
-# from plenario.utils.helpers import mail, slugify as slug
-# from plenario.settings import PLENARIO_SENTRY_URL
-#
-#
-# # Unless PLENARIO_SENTRY_URL specified in settings, don't try to start raven.
-# sentry = None
-# if PLENARIO_SENTRY_URL:
-#     sentry = Sentry(dsn=PLENARIO_SENTRY_URL)
-#
-#
-# def create_app():
-#     app = Flask(__name__)
-#     app.config.from_object('plenario.settings')
-#     app.url_map.strict_slashes = False
-#     login_manager.init_app(app)
-#     login_manager.login_view = "auth.login"
-#     bcrypt.init_app(app)
-#     mail.init_app(app)
-#
-#     if sentry:
-#         sentry.init_app(app)
-#     app.register_blueprint(api)
-#     app.register_blueprint(views)
-#     app.register_blueprint(auth)
-#     cache.init_app(app)
-#
-#     @app.before_request
-#     def check_maintenance_mode():
-#         """
-#         If maintenance mode is turned on in settings.py,
-#         Disable the API and the interactive pages in the explorer.
-#         """
-#         maint = app.config.get('MAINTENANCE')
-#         maint_pages = ['/v1/api', '/explore', '/admin']
-#
-#         maint_on = False
-#         for m in maint_pages:
-#             if m in request.path:
-#                 maint_on = True
-#
-#         if maint and maint_on and request.path != url_for('views.maintenance'):
-#             return redirect(url_for('views.maintenance'))
-#
-#     @app.teardown_appcontext
-#     def shutdown_session(exception=None):
-#         db_session.remove()
-#
-#     @app.errorhandler(404)
-#     def page_not_found(e):
-#         return render_template('404.html'), 404
-#
-#     @app.errorhandler(500)
-#     def page_not_found(e):
-#         return render_template('error.html'), 500
-#
-#     @app.template_filter('slugify')
-#     def slugify(s):
-#         return slug(s)
-#
-#     @app.template_filter('format_number')
-#     def reverse_filter(s):
-#         return '{:,}'.format(s)
-#
-#     @app.template_filter('format_date_sort')
-#     def reverse_filter(s):
-#         if s:
-#             return s.strftime('%Y%m%d%H%M')
-#         else:
-#             return '0'
-#
-#     @app.template_filter('has_description')
-#     def has_description(list_of_cols):
-#         try:
-#             # Any description attribute filled?
-#             return any([col['description'] for col in list_of_cols])
-#         except KeyError:
-#             # Is there even a description attribute?
-#             return False
-#
-#     return app
+from flask import Flask, render_template, redirect, url_for, request
+from raven.contrib.flask import Sentry
+from plenario.database import session as db_session
+from plenario.models import bcrypt
+from plenario.api import api, cache
+from plenario.auth import auth, login_manager
+from plenario.views import views
+from plenario.utils.helpers import mail, slugify as slug
+from plenario.settings import PLENARIO_SENTRY_URL
+
+
+# Unless PLENARIO_SENTRY_URL specified in settings, don't try to start raven.
+sentry = None
+if PLENARIO_SENTRY_URL:
+    sentry = Sentry(dsn=PLENARIO_SENTRY_URL)
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('plenario.settings')
+    app.url_map.strict_slashes = False
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+    bcrypt.init_app(app)
+    mail.init_app(app)
+
+    if sentry:
+        sentry.init_app(app)
+    app.register_blueprint(api)
+    app.register_blueprint(views)
+    app.register_blueprint(auth)
+    cache.init_app(app)
+
+    @app.before_request
+    def check_maintenance_mode():
+        """
+        If maintenance mode is turned on in settings.py,
+        Disable the API and the interactive pages in the explorer.
+        """
+        maint = app.config.get('MAINTENANCE')
+        maint_pages = ['/v1/api', '/explore', '/admin']
+
+        maint_on = False
+        for m in maint_pages:
+            if m in request.path:
+                maint_on = True
+
+        if maint and maint_on and request.path != url_for('views.maintenance'):
+            return redirect(url_for('views.maintenance'))
+
+    @app.teardown_appcontext
+    def shutdown_session(exception=None):
+        db_session.remove()
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
+
+    @app.errorhandler(500)
+    def page_not_found(e):
+        return render_template('error.html'), 500
+
+    @app.template_filter('slugify')
+    def slugify(s):
+        return slug(s)
+
+    @app.template_filter('format_number')
+    def reverse_filter(s):
+        return '{:,}'.format(s)
+
+    @app.template_filter('format_date_sort')
+    def reverse_filter(s):
+        if s:
+            return s.strftime('%Y%m%d%H%M')
+        else:
+            return '0'
+
+    @app.template_filter('has_description')
+    def has_description(list_of_cols):
+        try:
+            # Any description attribute filled?
+            return any([col['description'] for col in list_of_cols])
+        except KeyError:
+            # Is there even a description attribute?
+            return False
+
+    return app
 
--- plenario/settings.py
+++ plenario/settings.py
@@ -1,11 +1,11 @@
 from os import environ
 get = environ.get
 
-# SECRET_KEY = get('SECRET_KEY', 'abcdefghijklmnop')
-# PLENARIO_SENTRY_URL = get('PLENARIO_SENTRY_URL', None)
-# CELERY_SENTRY_URL = get('CELERY_SENTRY_URL', None)
-# DATA_DIR = '/tmp'
-#
+SECRET_KEY = get('SECRET_KEY', 'abcdefghijklmnop')
+PLENARIO_SENTRY_URL = get('PLENARIO_SENTRY_URL', None)
+CELERY_SENTRY_URL = get('CELERY_SENTRY_URL', None)
+DATA_DIR = '/tmp'
+
 DB_USER = get('DB_USER', 'postgres')
 DB_PASSWORD = get('DB_PASSWORD', '')
 DB_HOST = get('DB_HOST', 'localhost')
@@ -15,44 +15,44 @@ DB_NAME = get('DB_NAME', 'plenario_test')
 DATABASE_CONN = 'postgresql://{}:{}@{}:{}/{}'.\
     format(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
 
+REDIS_HOST = get('REDIS_HOST', 'localhost')
+
 # See: https://pythonhosted.org/Flask-Cache/#configuring-flask-cache
 # for config options
 CACHE_CONFIG = {
     'CACHE_TYPE': 'redis',
-    'CACHE_REDIS_URL': get('CACHE_REDIS_URL', 'redis://localhost:6379/0'),
+    'CACHE_REDIS_HOST': REDIS_HOST,
     'CACHE_KEY_PREFIX': get('CACHE_KEY_PREFIX', 'plenario_app')
 }
 
-REDIS_HOST = get('REDIS_HOST', 'localhost')
+# Dict with attributes for a default web admin
+# Uncomment for local testing.
+DEFAULT_USER = {}
+# DEFAULT_USER = {
+#     'name': 'plenario_user',
+#     'email': 'youremail@example.com',
+#     'password': 'your password'
+# }
 
-# # Dict with attributes for a default web admin
-# # Uncomment for local testing.
-# DEFAULT_USER = {}
-# # DEFAULT_USER = {
-# #     'name': 'plenario_user',
-# #     'email': 'youremail@example.com',
-# #     'password': 'your password'
-# # }
-#
-# AWS_ACCESS_KEY = get('AWS_ACCESS_KEY', '')
-# AWS_SECRET_KEY = get('AWS_SECRET_KEY', '')
-# S3_BUCKET = get('S3_BUCKET', '')
-#
-# # Email address for notifying site administrators
-# # Expect comma-delimited list of emails.
-# email_list = get('ADMIN_EMAILS')
-# if email_list:
-#     ADMIN_EMAILS = email_list.split(',')
-# else:
-#     ADMIN_EMAILS = []
+AWS_ACCESS_KEY = get('AWS_ACCESS_KEY', '')
+AWS_SECRET_KEY = get('AWS_SECRET_KEY', '')
+S3_BUCKET = get('S3_BUCKET', '')
 
-# # For emailing users. ('MAIL_USERNAME' is an email address.)
-# MAIL_SERVER = get('MAIL_SERVER', 'smtp.gmail.com')
-# MAIL_PORT = 587
-# MAIL_USE_TLS = True
-# MAIL_DISPLAY_NAME = 'Plenar.io Team'
-# MAIL_USERNAME = get('MAIL_USERNAME', '')
-# MAIL_PASSWORD = get('MAIL_PASSWORD', '')
-#
-# # Toggle maintenence mode
-# MAINTENANCE = False
+# Email address for notifying site administrators
+# Expect comma-delimited list of emails.
+email_list = get('ADMIN_EMAILS')
+if email_list:
+    ADMIN_EMAILS = email_list.split(',')
+else:
+    ADMIN_EMAILS = []
+
+# For emailing users. ('MAIL_USERNAME' is an email address.)
+MAIL_SERVER = get('MAIL_SERVER', 'smtp.gmail.com')
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_DISPLAY_NAME = 'Plenar.io Team'
+MAIL_USERNAME = get('MAIL_USERNAME', '')
+MAIL_PASSWORD = get('MAIL_PASSWORD', '')
+
+# Toggle maintenence mode
+MAINTENANCE = False
