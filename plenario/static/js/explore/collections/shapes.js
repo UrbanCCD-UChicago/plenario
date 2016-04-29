a22e32021544d998592039b78d37a733b10760ee
Will Engler <Engler.Will@gmail.com>
2016-04-28 19:38:52 -0500

    Point explore template to S3 bucket

--- plenario/static/js/explore/collections/shapes.js
+++
@@ -1,9 +0,0 @@
-var app = app || {};
-
-app.Shapes = Backbone.Collection.extend({
-    model: app.Shape,
-    url: '/v1/api/shapes/',
-    parse: function(data){
-        return data.objects;
-    }
-});
