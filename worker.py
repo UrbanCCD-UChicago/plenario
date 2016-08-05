fad7d2a6bf97a3ef810faa1d632ad82b25a290ad
HeyZoos <jab347@pitt.edu>
2016-08-05 11:09:08 -0500

    Removed unnecessary if-else nesting.

--- worker.py
+++ worker.py
@@ -77,192 +77,196 @@ if __name__ == "__main__":
             while worker_boss['do_work']:
 
                 if throttle < 0:
-                            check_in(birthtime, worker_id)
-                            throttle = 5
+                    check_in(birthtime, worker_id)
+                    throttle = 5
                 throttle -= 1
 
                 response = JobsQueue.get_messages(message_attributes=["ticket"])
-                if len(response) > 0:
-                    job = response[0]
-                    body = job.get_body()
+                # Check the length of the available queue messages to determine
+                # if there's work. If there's no work, sleep and do not attempt
+                # to perform any query logic.
+                if not len(response) > 0:
+                    time.sleep(wait_interval)
+                    continue
 
-                    # We need this check to avoid extraneous jobs
-                    # submitted by AWS Elastic Beanstalk.
-                    if not body == "plenario_job":
-                        log("Message is not a Plenario Job. Skipping.", worker_id)
-                        continue
+                job = response[0]
+                body = job.get_body()
 
-                    try:
-                        ticket = str(job.message_attributes["ticket"]["string_value"])
-                    except KeyError:
-                        log("ERROR: Job does not contain a ticket! Removing.", worker_id)
+                # Check to avoid extraneous jobs submitted by AWS Elastic Beanstalk.
+                if not body == "plenario_job":
+                    log("Message is not a Plenario Job. Skipping.", worker_id)
+                    continue
+
+                # Check that the job contains a valid ticket.
+                try:
+                    ticket = str(job.message_attributes["ticket"]["string_value"])
+                except KeyError:
+                    log("ERROR: Job does not contain a ticket! Removing.", worker_id)
+                    JobsQueue.delete_message(job)
+                    continue
+
+                # Test that the ticket is not malformed in some way.
+                try:
+                    status = get_status(ticket)
+                    assert status["status"] is not None
+                    assert status["meta"] is not None
+                except Exception as e:
+                    log("ERROR: Job is malformed ({}). Removing.".format(e), worker_id)
+                    JobsQueue.delete_message(job)
+                    continue
+
+                # All checks passed, this is a valid job.
+                log("Received job with ticket {}.".format(ticket), worker_id)
+
+                # Handle orphaned jobs
+                is_processing = status["status"] == "processing"
+                is_longrunning = "longrunning" in get_request(ticket)
+                is_deferred = status["meta"].get("lastStartTime") is not None
+                # TODO: Make pretty.
+                is_expired = (datetime.now() - datetime.strptime(status["meta"]["startTime"], "%Y-%m-%d %H:%M:%S.%f")).total_seconds() > job_timeout
+                deferral_expired = (datetime.now() - datetime.strptime(status["meta"]["lastStartTime"], "%Y-%m-%d %H:%M:%S.%f")).total_seconds() > job_timeout
+                if (is_processing and
+                    not is_longrunning and
+                        ((not is_deferred and is_expired) or (is_deferred and deferral_expired))):
+
+                    status["meta"]["tries"] = status["meta"]["tries"] + 1 \
+                        if status["meta"].get("tries") else 1
+
+                    # Only try orphaned jobs again once
+                    if status["meta"]["tries"] > 1:
+                        error_msg = "Stalled task {}. Removing.".format(ticket)
+                        set_ticket_error(status, ticket, error_msg, worker_id)
+
                         JobsQueue.delete_message(job)
-                        continue
-
-                    log("Received job with ticket {}.".format(ticket), worker_id)
-
-                    try:
-                        status = get_status(ticket)
-                        assert status["status"] is not None
-                        assert status["meta"] is not None
-                    except Exception as e:
-                                      log("ERROR: Job is malformed ({}). Removing.".format(e), worker_id)
-                                      JobsQueue.delete_message(job)
-                                      continue
-
-                    # Handle orphaned jobs
-                    is_processing = status["status"] == "processing"
-                    is_longrunning = "longrunning" in get_request(ticket)
-                    is_deferred = status["meta"].get("lastStartTime") is not None
-                    # TODO: Make pretty.
-                    is_expired = (datetime.now() - datetime.strptime(status["meta"]["startTime"], "%Y-%m-%d %H:%M:%S.%f")).total_seconds() > job_timeout
-                    deferral_expired = (datetime.now() - datetime.strptime(status["meta"]["lastStartTime"], "%Y-%m-%d %H:%M:%S.%f")).total_seconds() > job_timeout
-                    if (is_processing and
-                        not is_longrunning and
-                            ((not is_deferred and is_expired) or (is_deferred and deferral_expired))):
-
-                        status["meta"]["tries"] = status["meta"]["tries"] + 1 \
-                            if status["meta"].get("tries") else 1
-
-                        # Only try orphaned jobs again once
-                        if status["meta"]["tries"] > 1:
-                            error_msg = "Stalled task {}. Removing.".format(ticket)
-                            set_ticket_error(status, ticket, error_msg, worker_id)
-
-                            JobsQueue.delete_message(job)
-                            deregister_job(birthtime, worker_id)
-
-                            worker_boss['active_worker_count'] -= 1
-                            update_instance_protection(worker_boss, autoscaling_client)
-
-                            continue
-                        else:
-                            log("WARNING: Ticket {} has been orphaned...retrying.".format(ticket), worker_id)
-                            status["meta"]["lastDeferredTime"] = str(datetime.now())
-                            set_status(ticket, status)
-                        traceback.print_exc()
-
-                    # Standard job mutex
-                    elif status["status"] != "queued":
-                        log("Job has already been started. Skipping.", worker_id)
-                        continue
-
-                    status["status"] = "processing"
-
-                    if "lastDeferredTime" in status["meta"]:
-                        status["meta"]["lastResumeTime"] = str(datetime.now())
-                    else:
-                        status["meta"]["startTime"] = str(datetime.now())
-
-                    if "workers" not in status["meta"]:
-                        status["meta"]["workers"] = []
-
-                    status["meta"]["workers"].append(worker_id)
-                    set_status(ticket, status)
-
-                    # =========== Do work on query =========== #
-                    try:
-                        log("Starting work on ticket {}.".format(ticket), worker_id)
-                        register_job(ticket, birthtime, worker_id)
-
-                        req = get_request(ticket)
-                        endpoint = req['endpoint']
-                        query_args = req['query']
-
-                        if endpoint in endpoint_logic:
-
-                            # Add worker metadata
-                            query_args["jobsframework_ticket"] = ticket
-                            query_args["jobsframework_workerid"] = worker_id
-                            query_args["jobsframework_workerbirthtime"] = birthtime
-
-                            # Because we're getting serialized arguments from Redis,
-                            # we need to convert them back into a validated form.
-                            convert(query_args)
-                            query_args = ValidatorProxy(query_args)
-
-                            log("Ticket {}: endpoint {}.".format(ticket, endpoint), worker_id)
-
-                            result = endpoint_logic[endpoint](query_args)
-
-                            # Check for metacommands
-                            if "jobsframework_metacommands" in result:
-                                defer = False
-                                stop = False
-                                for command in result["jobsframework_metacommands"]:
-                                    if "setTimeout" in command:
-                                        job.change_visibility(command["setTimeout"])
-                                    elif "defer" in command:
-                                        status = get_status(ticket)
-                                        status["status"] = "queued"
-                                        status["meta"]["lastDeferredTime"] = str(datetime.now())
-                                        set_status(ticket, status)
-                                        log("Deferred work on ticket {}.".format(ticket), worker_id)
-                                        defer = True
-                                    elif "resubmit" in command:
-                                        submit_job(req)
-                                        log("Resubmitted job that was in ticket {}.".format(ticket), worker_id)
-                                        stop = True
-                                if stop:
-                                    JobsQueue.delete_message(job)
-                                    continue
-                                if defer:
-                                    continue
-
-                        elif endpoint in shape_logic:
-                            convert(query_args)
-                            query_args = ValidatorProxy(query_args)
-                            result = shape_logic[endpoint](query_args)
-                            if endpoint == 'aggregate-point-data' and query_args.data.get('data_type') != 'csv':
-                                result = convert_result_geoms(result)
-
-                        elif endpoint in etl_logic:
-                            if endpoint in ('update_weather', 'update_metar'):
-                                result = etl_logic[endpoint]()
-                            else:
-                                result = etl_logic[endpoint](query_args)
-
-                        else:
-                            raise ValueError("Attempting to send a job to an "
-                                             "invalid endpoint ->> {}"
-                                             .format(endpoint))
-
-                        set_result(ticket, result)
-                        status = get_status(ticket)
-                        status["status"] = "success"
-                        status["meta"]["endTime"] = str(datetime.now())
-                        set_status(ticket, status)
-
-                        log("Finished work on ticket {}.".format(ticket), worker_id)
-                        JobsQueue.delete_message(job)
-                    except Exception as e:
-                        traceback.print_exc()
-
-                        status = get_status(ticket)
-                        if status['meta'].get('tries'):
-                            status['meta']['tries'] += 1
-                        else:
-                            status['meta']['tries'] = 1
-
-                        # Try failed jobs twice
-                        if status["meta"]["tries"] > 2:
-                            error_msg = "{} errored with {}.".format(ticket, e)
-                            set_ticket_error(status, ticket, error_msg, worker_id)
-                            JobsQueue.delete_message(job)
-                        else:
-                            status["status"] = "queued"
-                            status["meta"]["lastDeferredTime"] = str(datetime.now())
-                            log("ERROR: Ticket {} errored with: {}...retrying.".format(ticket, e), worker_id)
-                            set_status(ticket, status)
-
-                    finally:
                         deregister_job(birthtime, worker_id)
+
                         worker_boss['active_worker_count'] -= 1
                         update_instance_protection(worker_boss, autoscaling_client)
 
+                        continue
+                    else:
+                        log("WARNING: Ticket {} has been orphaned...retrying.".format(ticket), worker_id)
+                        status["meta"]["lastDeferredTime"] = str(datetime.now())
+                        set_status(ticket, status)
+                    traceback.print_exc()
+
+                # Standard job mutex
+                elif status["status"] != "queued":
+                    log("Job has already been started. Skipping.", worker_id)
+                    continue
+
+                status["status"] = "processing"
+
+                if "lastDeferredTime" in status["meta"]:
+                    status["meta"]["lastResumeTime"] = str(datetime.now())
                 else:
-                    # No work! Idle for a bit to save compute cycles.
-                    time.sleep(wait_interval)
+                    status["meta"]["startTime"] = str(datetime.now())
+
+                if "workers" not in status["meta"]:
+                    status["meta"]["workers"] = []
+
+                status["meta"]["workers"].append(worker_id)
+                set_status(ticket, status)
+
+                # =========== Do work on query =========== #
+                try:
+                    log("Starting work on ticket {}.".format(ticket), worker_id)
+                    register_job(ticket, birthtime, worker_id)
+
+                    req = get_request(ticket)
+                    endpoint = req['endpoint']
+                    query_args = req['query']
+
+                    if endpoint in endpoint_logic:
+
+                        # Add worker metadata
+                        query_args["jobsframework_ticket"] = ticket
+                        query_args["jobsframework_workerid"] = worker_id
+                        query_args["jobsframework_workerbirthtime"] = birthtime
+
+                        # Because we're getting serialized arguments from Redis,
+                        # we need to convert them back into a validated form.
+                        convert(query_args)
+                        query_args = ValidatorProxy(query_args)
+
+                        log("Ticket {}: endpoint {}.".format(ticket, endpoint), worker_id)
+
+                        result = endpoint_logic[endpoint](query_args)
+
+                        # Check for metacommands
+                        if "jobsframework_metacommands" in result:
+                            defer = False
+                            stop = False
+                            for command in result["jobsframework_metacommands"]:
+                                if "setTimeout" in command:
+                                    job.change_visibility(command["setTimeout"])
+                                elif "defer" in command:
+                                    status = get_status(ticket)
+                                    status["status"] = "queued"
+                                    status["meta"]["lastDeferredTime"] = str(datetime.now())
+                                    set_status(ticket, status)
+                                    log("Deferred work on ticket {}.".format(ticket), worker_id)
+                                    defer = True
+                                elif "resubmit" in command:
+                                    submit_job(req)
+                                    log("Resubmitted job that was in ticket {}.".format(ticket), worker_id)
+                                    stop = True
+                            if stop:
+                                JobsQueue.delete_message(job)
+                                continue
+                            if defer:
+                                continue
+
+                    elif endpoint in shape_logic:
+                        convert(query_args)
+                        query_args = ValidatorProxy(query_args)
+                        result = shape_logic[endpoint](query_args)
+                        if endpoint == 'aggregate-point-data' and query_args.data.get('data_type') != 'csv':
+                            result = convert_result_geoms(result)
+
+                    elif endpoint in etl_logic:
+                        if endpoint in ('update_weather', 'update_metar'):
+                            result = etl_logic[endpoint]()
+                        else:
+                            result = etl_logic[endpoint](query_args)
+
+                    else:
+                        raise ValueError("Attempting to send a job to an "
+                                         "invalid endpoint ->> {}"
+                                         .format(endpoint))
+
+                    set_result(ticket, result)
+                    status = get_status(ticket)
+                    status["status"] = "success"
+                    status["meta"]["endTime"] = str(datetime.now())
+                    set_status(ticket, status)
+
+                    log("Finished work on ticket {}.".format(ticket), worker_id)
+                    JobsQueue.delete_message(job)
+                except Exception as e:
+                    traceback.print_exc()
+
+                    status = get_status(ticket)
+                    if status['meta'].get('tries'):
+                        status['meta']['tries'] += 1
+                    else:
+                        status['meta']['tries'] = 1
+
+                    # Try failed jobs twice
+                    if status["meta"]["tries"] > 2:
+                        error_msg = "{} errored with {}.".format(ticket, e)
+                        set_ticket_error(status, ticket, error_msg, worker_id)
+                        JobsQueue.delete_message(job)
+                    else:
+                        status["status"] = "queued"
+                        status["meta"]["lastDeferredTime"] = str(datetime.now())
+                        log("ERROR: Ticket {} errored with: {}...retrying.".format(ticket, e), worker_id)
+                        set_status(ticket, status)
+
+                finally:
+                    deregister_job(birthtime, worker_id)
+                    worker_boss['active_worker_count'] -= 1
+                    update_instance_protection(worker_boss, autoscaling_client)
 
         log("Exited run loop. Goodbye!", worker_id)
         deregister_worker(worker_id)
@@ -284,18 +288,6 @@ if __name__ == "__main__":
         t.join(5)
         if not t.is_alive():
             threads.pop(looper)
-            # If threads exit prematurely, then replace them.
-            if worker_boss['do_work']:
-                log("ERROR: A WORKER DIED PREMATURELY! REPLACING.".format(len(threads)), "WORKER BOSS")
-                print("====================== WORKER BOSS STATUS: ======================")
-                print check_output(["cat", "/proc/{}/status".format(os.getpid())])
-                print("====================== WORKER BOSS MEMDUMP: ======================")
-                print check_output(["cat", "/proc/{}/maps".format(os.getpid())])
-                worker_boss['active_worker_count'] -= 1
-                t = threading.Thread(target=worker, name="plenario-worker-thread")
-                t.daemon = False
-                threads.append(t)
-                t.start()
 
     session.close()
     log("All workers have exited.", "WORKER BOSS")
