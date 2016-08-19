324c9c273dd706e6e9482324e1ae0e4bdc7ca479
HeyZoos <jab347@pitt.edu>
2016-08-19 08:43:02 -0500

    Uncomment auth and login registration for app

--- plenario/__init__.py
+++ plenario/__init__.py
@@ -36,15 +36,15 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('plenario.settings')
     app.url_map.strict_slashes = False
-    # login_manager.init_app(app)
-    # login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
     bcrypt.init_app(app)
 
     if sentry:
         sentry.init_app(app)
     app.register_blueprint(api)
     app.register_blueprint(views)
-    # app.register_blueprint(auth)
+    app.register_blueprint(auth)
     cache.init_app(app)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONN
