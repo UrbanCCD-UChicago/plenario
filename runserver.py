320f26632ee3462aaeb1a88fcdcfe69dbbb2322b
HeyZoos <jab347@pitt.edu>
2016-12-15 08:47:15 -0600

    Move 'application' outside of if '__main__' -.-

--- runserver.py
+++ runserver.py
@@ -1,4 +1,5 @@
+from plenario import create_app
+application = create_app()
+
 if __name__ == "__main__":
-    from plenario import create_app
-    application = create_app()
     application.run(host="0.0.0.0")
