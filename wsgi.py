e2784633bf360074823fead1efa440e6f6ee3552
HeyZoos <jab347@pitt.edu>
2016-12-15 10:33:20 -0600

    Add conditions to wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,12 @@
-from plenario import create_app
+from os import getenv
 
-application = create_app()
+from plenario import create_app as server
+from plenario.update import create_worker as worker
+
+
+apps = {
+    "server": server,
+    "worker": worker
+}
+
+application = apps["worker"] if getenv("WORKER", None) else apps["server"]
