f487077cc76e00d5d28e0c2a1326158fe30cc97c
heyzoos <jbracho@uchicago.edu>
2017-05-19 13:13:44 -0500

    Modify shape etl to replace existing table

--- plenario/etl/shape.py
+++ plenario/etl/shape.py
@@ -1,22 +1,9 @@
-# -*- coding: utf-8 -*-
-
 import zipfile
 
-from plenario.database import postgres_session, postgres_engine as engine
-from plenario.etl.common import ETLFile, PlenarioETLError, add_unique_hash,\
-    delete_absent_hashes
-from plenario.utils.shapefile import import_shapefile, ShapefileError
-from sqlalchemy import Table, MetaData, select
-
-
-def reflect(table_name):
-    """
-    Given the name of a table present in the database,
-    return a SQLAlchemy Table object that represents it.
-    :param table_name:
-    """
-    return Table(table_name, MetaData(),
-                 autoload_with=engine, extend_existing=True)
+from sqlalchemy.exc import ProgrammingError
+from plenario.database import postgres_engine, postgres_session
+from plenario.etl.common import ETLFile, add_unique_hash
+from plenario.utils.shapefile import import_shapefile
 
 
 class ShapeETL:
@@ -28,89 +15,25 @@ class ShapeETL:
         self.meta = meta
 
     def add(self):
-        if self.meta.is_ingested:
-            raise PlenarioETLError("Table {} has already been ingested.".
-                                   format(self.table_name))
-        
-        new = HashedShape(self.table_name, self.source_url, self.source_path)
-        try:
-            new.ingest()
-            self.meta.update_after_ingest()
-            postgres_session.commit()
-        except:
-            # In case ingestion failed partway through,
-            # be sure to leave no trace.
-            new.drop()
-            raise
+        staging_name = 'staging_{}'.format(self.table_name)
 
-    def update(self):
-        assert self.meta.is_ingested
-        existing = reflect(self.table_name)
-        staging_name = 's_' + self.table_name
-
-        with HashedShape(staging_name, self.source_url,
-                         self.source_path) as staging:
-            self._hash_update(staging, existing)
-
-        self.meta.update_after_ingest()
-
-    @staticmethod
-    def _hash_update(staging, existing):
-        delete_absent_hashes(staging.name, existing.name)
-
-        # Ensures column alignment in upcoming selection and insertion
-        column_names = sorted([col.name for col in existing.columns])
-        columns = [staging.c[name] for name in column_names]
-
-        # Select all records from staging
-        # whose hashes are not present in existing.
-        join_condition = staging.c['hash'] == existing.c['hash']
-        selection = select(columns).select_from(
-            staging.outerjoin(existing, join_condition)
-        ).where(existing.c['hash'] == None)
-
-        # Insert those into existing
-        insertion = existing.insert().from_select(column_names, selection)
-        try:
-            engine.execute(insertion)
-        except Exception as e:
-            raise PlenarioETLError(repr(e) + '\n' + str(selection))
-
-
-class HashedShape(object):
-    """
-    Ingest a shapefile,
-    and append an md5 hash column.
-    """
-
-    def __init__(self, name, url, path=None):
-        self.name = name
-        self.url = url
-        self.path = path
-
-    def ingest(self):
-        """
-        Create the table. The caller is responsible for cleanup.
-        :return: SQLAlchemy Table
-        """
-
-        with ETLFile(source_url=self.url, source_path=self.path, interpret_as='bytes') as file_helper:
+        with ETLFile(self.source_path, self.source_url, interpret_as='bytes') as file_helper:
             handle = open(file_helper.handle.name, "rb")
             with zipfile.ZipFile(handle) as shapefile_zip:
-                import_shapefile(shapefile_zip, self.name)
+                import_shapefile(shapefile_zip, staging_name)
+                add_unique_hash(staging_name)
 
-        add_unique_hash(self.name)
-        return reflect(self.name)
+        try:
+            postgres_engine.execute('drop table {}'.format(self.table_name))
+        except ProgrammingError:
+            pass
 
-    def __enter__(self):
-        """
-        If user of this class does not want to be responsible for cleanup,
-        use the class as a context manager.
-        """
-        return self.ingest()
+        rename_table = 'alter table {} rename to {}'
+        rename_table = rename_table.format(staging_name, self.table_name)
+        postgres_engine.execute(rename_table)
+        
+        self.meta.update_after_ingest()
+        postgres_session.commit()
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.drop()
-
-    def drop(self):
-        engine.execute("DROP TABLE IF EXISTS {};".format(self.name))
+    def update(self):
+        self.add()
