e39a9cd60556f43d07d7000ec7b5fdf5db30f5ca
Vince Forgione <v.forgione@gmail.com>
2017-07-07 11:26:35 -0500

    linted ./manage.py

--- manage.py
+++ manage.py
@@ -1,103 +1,112 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
+import logging
 import os
 import signal
-import sqlalchemy.exc
 import subprocess
 import warnings
+from os import getenv
+from time import sleep
 
+import sqlalchemy.exc
 from flask.exthook import ExtDeprecationWarning
 from flask_script import Manager
 from kombu.exceptions import OperationalError
-from os import getenv
+from sqlalchemy import create_engine
 from sqlalchemy.exc import IntegrityError, ProgrammingError
-from time import sleep
 
 from plenario import create_app as server
-from plenario.settings import DATABASE_CONN, REDSHIFT_CONN, DB_NAME
-from plenario.settings import DEFAULT_USER
+from plenario.database import create_database, create_extension, postgres_base, psql, postgres_session, \
+        drop_database, postgres_engine as plenario_engine
+from plenario.models.User import User
+from plenario.settings import DATABASE_CONN, REDSHIFT_CONN, DB_NAME, DEFAULT_USER
+from plenario.tasks import health
+from plenario.utils.weather import WeatherETL, WeatherStationsETL
 from plenario.worker import create_worker as worker
 
 
+logger = logging.getLogger('manage.py')
+logger.setLevel(logging.DEBUG)
+
+
 # Ignore warnings stating that libraries we depend on use deprecated flask code
-warnings.filterwarnings("ignore", category=ExtDeprecationWarning)
+warnings.filterwarnings('ignore', category=ExtDeprecationWarning)
 # Ignore warnings stating that our forms do not address all model fields
-warnings.filterwarnings("ignore", "Fields missing from ruleset", UserWarning)
+warnings.filterwarnings('ignore', 'Fields missing from ruleset', UserWarning)
 
 
 apps = {
-    "server": server,
-    "worker": worker
+    'server': server,
+    'worker': worker
 }
 
-application = apps["worker"]() if getenv("WORKER", None) else apps["server"]()
+if getenv('WORKER', None):
+    application = apps['worker']()
+else:
+    application = apps['server']()
 manager = Manager(application)
 
 
 @manager.command
 def runserver():
-    """Start up plenario server."""
-
-    application.run(host="0.0.0.0", port=5000, debug=os.environ.get('DEBUG'))
+    """Start up plenario server.
+    """
+    application.run(host='0.0.0.0', port=5000, debug=os.environ.get('DEBUG'))
 
 
 @manager.command
 def worker():
-    """Start up celery worker."""
-
-    celery_commands = ["celery", "-A", "plenario.tasks", "worker", "-l", "INFO"]
+    """Start up celery worker.
+    """
+    celery_commands = ['celery', '-A', 'plenario.tasks', 'worker', '-l', 'INFO']
     wait(subprocess.Popen(celery_commands))
 
 
 @manager.command
 def monitor():
-    """Start up flower task monitor."""
-
-    flower_commands = ["flower", "-A", "plenario.tasks", "--persistent"]
+    """Start up flower task monitor.
+    """
+    flower_commands = ['flower', '-A', 'plenario.tasks', '--persistent']
     wait(subprocess.Popen(flower_commands))
 
 
 @manager.command
 def pg():
-    """Psql into postgres."""
-
-    print("[plenario] Connecting to %s" % DATABASE_CONN)
-    wait(subprocess.Popen(["psql", DATABASE_CONN]))
+    """Psql into postgres.
+    """
+    logger.debug('[plenario] Connecting to %s' % DATABASE_CONN)
+    wait(subprocess.Popen(['psql', DATABASE_CONN]))
 
 
 @manager.command
 def rs():
-    """Psql into redshift."""
-
-    print("[plenario] Connecting to %s" % REDSHIFT_CONN)
-    wait(subprocess.Popen(["psql", REDSHIFT_CONN]))
+    """Psql into redshift.
+    """
+    logger.debug('[plenario] Connecting to %s' % REDSHIFT_CONN)
+    wait(subprocess.Popen(['psql', REDSHIFT_CONN]))
 
 
 @manager.command
 def test():
-    """Run nosetests."""
-
-    nose_commands = ["nosetests", "-s", "tests", "-vv"]
+    """Run nosetests.
+    """
+    nose_commands = ['nosetests', '-s', 'tests', '-vv']
     wait(subprocess.Popen(nose_commands))
 
 
-@manager.command
-def config():
-    """Set up environment variables for plenario."""
-
-    pass
+# @manager.command
+# def config():
+#     """Set up environment variables for plenario."""
+#     pass
 
 
 @manager.command
 def init():
-    """Initialize the database."""
-
+    """Initialize the database.
+    """
     # TODO(heyzoos)
     # Check for dependencies to fail fast and helpfully before running:
     #   - postgresql-client
-
-    from plenario.database import create_database
-    from sqlalchemy import create_engine
 
     base_uri = DATABASE_CONN.rsplit('/', 1)[0]
     base_engine = create_engine(base_uri)
@@ -109,66 +118,52 @@ def init():
             create_database(base_engine, DB_NAME)
             break
         except ProgrammingError:
-            print('[plenario] It already exists!')
+            logger.debug('[plenario] It already exists!')
             break
         except sqlalchemy.exc.OperationalError:
-            print('[plenario] Database has not started yet.')
+            logger.debug('[plenario] Database has not started yet.')
             sleep(interval)
-
-    from plenario.database import create_extension
-    from plenario.database import postgres_engine as plenario_engine, postgres_base
-    from plenario.utils.weather import WeatherETL, WeatherStationsETL
 
     try:
         create_extension(plenario_engine, 'postgis')
         create_extension(plenario_engine, 'plv8')
     except ProgrammingError:
-        print('[plenario] It already exists!')
+        logger.debug('[plenario] It already exists!')
 
-    print('[plenario] Creating metadata tables')
+    logger.debug('[plenario] Creating metadata tables')
     postgres_base.metadata.create_all()
 
-    print('[plenario] Creating weather tables')
+    logger.debug('[plenario] Creating weather tables')
     WeatherStationsETL().make_station_table()
     WeatherETL().make_tables()
 
-    from plenario.database import psql
-
     # Set up custom functions, triggers and views in postgres
-    psql("./plenario/dbscripts/sensor_tree.sql")
-    psql("./plenario/dbscripts/point_from_location.sql")
+    psql('./plenario/dbscripts/sensor_tree.sql')
+    psql('./plenario/dbscripts/point_from_location.sql')
 
     # Set up the default user if we are running in anything but production
     if os.environ.get('CONFIG') != 'prod':
-        from plenario.database import postgres_session
-        from plenario.models.User import User
-
-        print('[plenario] Create default user')
+        logger.debug('[plenario] Create default user')
         user = User(**DEFAULT_USER)
 
         try:
             postgres_session.add(user)
             postgres_session.commit()
         except IntegrityError:
-            print('[plenario] Already exists!')
+            logger.debug('[plenario] Already exists!')
             postgres_session.rollback()
-
-    from plenario.tasks import health
 
     # This will get celery to set up its meta tables
     try:
         health.delay()
     except OperationalError:
-        print('[plenario] Redis is not running!')
+        logger.debug('[plenario] Redis is not running!')
 
 
 @manager.command
 def uninstall():
-    """Drop the plenario databases."""
-
-    from sqlalchemy import create_engine
-    from plenario.database import drop_database
-
+    """Drop the plenario databases.
+    """
     base_uri = DATABASE_CONN.rsplit('/', 1)[0]
     base_engine = create_engine(base_uri)
     try:
@@ -185,8 +180,8 @@ def uninstall():
 
 
 def wait(process):
-    """Waits on a process and passes along sigterm."""
-
+    """Waits on a process and passes along sigterm.
+    """
     try:
         signal.pause()
     except (KeyboardInterrupt, SystemExit):
@@ -194,5 +189,5 @@ def wait(process):
         process.wait()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     manager.run()
